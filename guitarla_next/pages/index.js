e8886cc4b172f32730a2344a5ad61e5e5d03f114
ZaKaeDa <l2jerangon@gmail.com>
2022-06-23 14:05:24 -0400

    guitarla_nest

---
+++ guitarla_next/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+export default function Home() {
+    return (
+        <Layout>
+            <h1>Aprendiendo NextJs</h1>
+        </Layout>
+    )
+}
---
+++ guitarla_next/pages/nosotros.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const Nosotros = () => {
+    return (
+        <div>
+            <Layout>
+                <h1>Desde Nosotros</h1>
+            </Layout>
+        </div>
+    )
+}
+
+export default Nosotros
