fe4b86181a7dff6052626d8dfa81e4b1a08e22bd
ZaKaeDa <l2jerangon@gmail.com>
2022-07-02 11:00:38 -0400

    deployment

--- guitarla_next/components/Layout.js
+++ guitarla_next/components/Layout.js
@@ -10,7 +10,7 @@ const Layout = ( { children, pagina, guitarra } ) => {
                 <meta name="description" content="Sitio web de venta de Guitarras." />
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;800;900&family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap" />
             </Head>
 
